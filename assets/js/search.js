88ce96c25621735768e1c2d9701c16accdbf6403
qweasdzxcll <denispodmogilnyj@gmail.com>
2024-11-20 19:27:42 +0300

    На странице с достопримечательностями реализован поиск, пагинация, фильтрация. Доделана  страница с одной достопримечательностью. Есть адаптив под мобильные устройства

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,15 +1,13 @@
-let input = document.getElementById('input')
-let title = document.getElementById('title')
-const res = localStorage.getItem('data')
-const date = JSON.parse(res)
-document.getElementById("img1").src=date.img1
-document.getElementById("img2").src=date.img2
-document.getElementById("img3").src=date.img3
-document.getElementById("img4").src=date.img4
-document.getElementById('text').textContent = date.text
+const input = document.getElementById('input');
+const title = document.getElementById('title');
+const res = localStorage.getItem('data');
+const date = JSON.parse(res);
 
 
-function updateTitle(){
-    title.textContent = input.value;
-}
-
+document.getElementById("img1").src=date.img1;
+document.getElementById("img2").src=date.img2;
+document.getElementById("img3").src=date.img3;
+document.getElementById("img4").src=date.img4;
+document.getElementById('text').textContent = date.text;
+document.getElementById('title').textContent = date.title;
+const map = document.getElementById('map').innerHTML += `<iframe src='${date.map}' width="822" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
