a7151496c79adede9ae9d4ecf70a415d4fe34595
qweasdzxcll <denispodmogilnyj@gmail.com>
2024-11-20 19:11:53 +0300

    first commit

---
+++ assets/js/input.js
@@ -0,0 +1,46 @@
+const search = document.getElementById('input').value;
+const attr = document.querySelector('.main__item');
+const theatre = document.querySelector('.main__theatre');
+const palace = document.querySelector('.main__palace');
+const garden = document.querySelector('.main__garden');
+const beach = document.querySelector('.main__beach');
+const museum = document.querySelector('.main__museum');
+const tijuca = document.querySelector('.main__tijuca');
+const cathedral = document.querySelector('.main__cathedral');
+const christ = document.querySelector('.main__christ');
+
+const attractions = ['Christ the Redeemer Statue', 'Tiradentes Palace', 'Municipal Theater', 'Botanical Garden of Rio de Janeiro', 'Copacabana Beach', 'Tijuca National Park', 'Museum of Modern Art', 'Cathedral of Saint Sebastian']
+
+
+
+// // localStorage.setItem(JSON.stringify(attractions))
+
+// function chrst(search) {
+//     if (search.value == 'Christ the Redeemer Statue') {
+//         christ.style.display = 'block'
+//     }
+//     // for(let i=0; i<localStorage.length; i++) {
+//     //     let key = localStorage.key(i);
+//     //     if (search == 'Christ the Redeemer Statue') {
+//     //         christ.style.display = 'block'
+//     //     }
+//     //   }
+// }
+
+// function search() {
+//     let input = document.getElementById('input').value;
+//     input = input.toLowerCase()
+//     let a = document.querySelector('.main__item')
+//     for (let i = 0; i < a.length; i++) {
+//         if (x[i].innerHTML.toLowerCase().includes(input)) {
+//             x[i].style.display = 'none'
+//         }
+//         else {
+//             x[i].style.display = 'list-item'
+//         }
+//     }
+// }
+
+// function search(search) {
+//     if 
+// }
